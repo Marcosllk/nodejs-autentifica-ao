cfd136c22cfaea7ef42e2ca096b0f15903a42521
Marcos <marcosllk84@gmail.com>
2021-11-03 16:24:30 -0300

    corrigindo erro da rota

--- servidor/controllers/address.js
+++ servidor/controllers/address.js
@@ -1,6 +1,6 @@
 const userRoutes = require("../routes/usuarios");
 
-userRoutes("/") ListarEndereços async function  (req, res) {
+userRoutes("/ListarEndereços", async function  (req, res) {
   const data = await address.findAll();
   return res.json({ data })
-}
+})
